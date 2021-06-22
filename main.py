240d43f8f476040daf53c635fb1a5909a3cfccea
santoshm914 <santoshm914@gmail.com>
2021-06-22 20:31:18 +0530

    first commit for our application

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+
+def index():
+    return "<h1> this is flask app</h1>"
+
+
+if __name__=="__main__":
+    app.run()
+
+
+
+
+
